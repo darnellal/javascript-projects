e71f67f8d49f5a8d636f4a58dee06c346bc5e1ca
darnellal <dlasley5@hotmail.com>
2024-07-15 15:45:55 -0500

    Class 7 Exercises: Objects and the Math Object

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,13 +12,74 @@ let salamander = {
    age: 5
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
+let beagle = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+};
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+};
 
 // After you have created the other object literals, add the astronautID property to each one.
 
+superChimpOne['astronaut ID'] = 1;
+salamander['astronautID'] = 2;
+superChimpTwo['astronautID'] = 3;
+beagle['astronautID'] = 4;
+tardigrade['astronautID'] = 5;
+
+
 // Add a move method to each animal object
+
+superChimpOne['move'] = function () {return Math.round(Math.random()*10)};
+salamander['move'] = function () {return Math.round(Math.random()*10)};
+superChimpTwo['move'] = function () {return Math.round(Math.random()*10)};
+beagle['move'] = function () {return Math.round(Math.random()*10)};
+tardigrade['move'] = function () {return Math.round(Math.random()*10)};
 
 // Create an array to hold the animal objects.
 
+let crew = [superChimpOne, salamander, superChimpTwo, beagle, tardigrade];
+
 // Print out the relevant information about each animal.
 
+function crewReports (animal) {
+   return (`${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+} 
+
+for (item in crew) {
+   console.log(crewReports(crew[item]))
+};
+
 // Start an animal race!
+
+function fitnessTest () {
+   let animalRace = []
+   for (index in crew) { 
+      let stepsInRace = 0;
+      let turns = 0;
+      while (stepsInRace < 20) {
+         stepsInRace += crew[index].move();
+         turns += 1;
+      }
+      animalRace.push(`${crew[index].name} took ${turns} turns to take 20 steps.`);
+   } return animalRace;
+}
+
+let results = fitnessTest();
+for (item in results) {
+   console.log(results[item]);
+}
